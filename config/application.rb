6b29f8433ee6e9d043bb277122d60352e14c3e0a
Jaime García Méndez <jaimegarciamendez@gmail.com>
2024-06-23 20:37:38 -0300

    change app name

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module RailsApiAndReact
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework nil
+    end
   end
 end
