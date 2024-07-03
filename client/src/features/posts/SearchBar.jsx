7f3fe626303c585eaa27a1cdf03b918a717c6df3
Jaime García Méndez <jaimegarciamendez@gmail.com>
2024-07-03 10:24:09 -0300

    Done

--- client/src/features/posts/SearchBar.jsx
+++ client/src/features/posts/SearchBar.jsx
@@ -1,5 +1,8 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
+import { InputAdornment, TextField, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ClearIcon from '@mui/icons-material/Clear';
 
 function SearchBar({ value, onDebounceChange, onImmediateChange }) {
   const searchDebounce = useRef(null);
@@ -15,16 +18,49 @@ function SearchBar({ value, onDebounceChange, onImmediateChange }) {
     }, 500);
   }
 
+  const handleClear = () => {
+    onImmediateChange('');
+    onDebounceChange('');
+  };
+
   return (
-    <input
-      type="text"
+    <TextField
+      fullWidth
+      variant="outlined"
       value={value}
       onChange={handleSearchChange}
-      placeholder="Search..."
+      placeholder="Search posts..."
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon color="action" />
+          </InputAdornment>
+        ),
+        endAdornment: value && (
+          <InputAdornment position="end">
+            <IconButton onClick={handleClear} edge="end" size="small">
+              <ClearIcon />
+            </IconButton>
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        mb: 2,
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: 'rgba(0, 0, 0, 0.23)',
+          },
+          '&:hover fieldset': {
+            borderColor: 'rgba(0, 0, 0, 0.87)',
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: 'primary.main',
+          },
+        },
+      }}
     />
   );
 }
-
 SearchBar.propTypes = {
   value: PropTypes.string.isRequired,
   onDebounceChange: PropTypes.func.isRequired,
